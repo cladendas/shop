4a3d0807b9cdaf201195916ce422b5ca22911437
cladendas <rvdagi05@yandex.ru>
2017-11-30 19:04:25 +0300

    добавил комментарии в UserUI

--- UserUI.java
+++ UserUI.java
@@ -6,7 +6,9 @@ import java.util.Scanner;
 //класс для взаимодействия с пользователем
 public class UserUI {
 
+// ссылка на инстанс Tracker
     Tracker tracker = new Tracker();
+// ссылка на класс Scaner для возможности ввода пользоателем данных
     Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
@@ -16,25 +18,26 @@ public class UserUI {
     }
 
     public void init() {
-
+// вывод в консоль первого сообщения-указания для определения имени пользователя
         System.out.println("Представься...");
-
+// считываение введеных данных и занесение их в переменную name, по которой к пользователю будут обращаться, как по имени
         String name = scanner.nextLine();
+// вывод в консоль второго сообщения-указания для выбора дальнейших действий
+        System.out.printf("%s, что сделать?\n", name);
 
-        System.out.printf("%s, что сделать?\n", answer);
-
+// переменные для хранения пунктов меню
         String menuSection0 = ". Add\n";
         String menuSection1 = ". Remove\n";
         String menuSection2 = ". Change\n";
         String menuSection3 = ". Find\n";
         String menuSection4 = ". Exit\n";
-
+// переменная для хранения всех пунктов меню в сборе и с указанием номера каждого пункта
         String menuStart = 0 + menuSection0 + 1 + menuSection1 + 2 + menuSection2 + 3 + menuSection3 + 4 + menuSection4;
-
+// вывод в консоль меню и сообщения-указания "Select:"
         System.out.println(menuStart + "\nSelect:");
-
+// переменная для хранения введенного пользователем числа, указывающего на его выбор конкретного пункта меню
         int answerMenuStart = scanner.nextInt();
-
+// логика обработки введенного пользователем числа
         if(answerMenuStart > 4 || answerMenuStart < 0) {
             System.out.println("Необходимо указать цифру, соответствующую выбранному полю меню...");
             System.out.println(menuStart + "\nSelect:");
