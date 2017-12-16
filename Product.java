da3201e0440d79e4bddfd18954ab9705e6d5b566
cladendas <rvdagi05@yandex.ru>
2017-12-17 01:02:29 +0300

    добавил setId()

--- Product.java
+++ Product.java
@@ -40,6 +40,10 @@ public class Product {
     public int getId() {
         return id;
     }
+    public void setId(int id) {
+        this.id = id;
+    }
+
 
     @Override
     public boolean equals(Object obj) {
