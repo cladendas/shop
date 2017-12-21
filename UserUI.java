043a3be133e159a1b970eabfddb66673fae6a2f7
cladendas <rvdagi05@yandex.ru>
2017-12-21 23:50:11 +0300

    навел визуальный порядок, реализоал обработку исключения, если пользователь ввел не число при выборе пункта меню, часть изменений относятся к экспериментам при доработке методов работы с элементами массива products

--- UserUI.java
+++ UserUI.java
@@ -1,17 +1,21 @@
 package shop;
 
+// класс для обработки исключения, отвечающего за несоответствие введеного типа
+import java.util.InputMismatchException;
 // класс для считывания введных данных с консоли
 import java.util.Scanner;
 
 //класс для взаимодействия с пользователем
 public class UserUI {
 
-// ссылка на инстанс Tracker
-   Tracker tracker = new Tracker();
-// ссылка на класс Scaner для возможности ввода пользоателем данных
-   Scanner scanner = new Scanner(System.in);
+    // ссылка на инстанс Tracker
+    Tracker tracker = new Tracker();
 
-// переменные для хранения пунктов меню
+    // ссылка на класс Scaner для возможности ввода пользоателем данных
+    Scanner scanner = new Scanner(System.in);
+
+
+    // переменные для хранения пунктов меню
     String menuSectionADD = ". Add\n";
     String menuSectionREMOVE = ". Remove\n";
     String menuSectionCHANGE = ". Change\n";
@@ -20,21 +24,22 @@ public class UserUI {
     String menuSectionEXIT = ". Exit\n";
 
     //переменная для хранения ключа меню ADD - добавление продукта
-    private static final int ADD = 0;
+    final int ADD = 0;
     //переменная для хранения ключа меню REMOVE - удаление продукта
-    private static final int REMOVE = 1;
+    final int REMOVE = 1;
     //переменная для хранения ключа CHANGE - изменение продукта
-    private static final int CHANGE = 2;
+    final int CHANGE = 2;
     //переменная для хранения ключа FIND - поиск продукта
-    private static final int FIND = 3;
+    final int FIND = 3;
     //переменная для хранения ключа SHOWALL - вывод всех продуктов в консоль
-    private static final int SHOWALL = 4;
+    final int SHOWALL = 4;
     //переменная для хранения ключа EXIT - выход из программы
-    private static final int EXIT = 5;
+    final int EXIT = 5;
 
     // переменная для хранения всех пунктов меню в сборе и с указанием номера каждого пункта
     String menuStart = ADD + menuSectionADD + REMOVE + menuSectionREMOVE + CHANGE + menuSectionCHANGE + FIND + menuSectionFIND +
             SHOWALL + menuSectionSHOWALL + EXIT + menuSectionEXIT;
+
 
     //переменные для хранения текстовых сообщений для пользователя
     String mustWrite = "Необходимо указать цифру, соответствующую выбранному полю меню...";
@@ -47,12 +52,8 @@ public class UserUI {
     String forThisShouldNameId = "Для этого действия необходимо ввести наименование продукта и его артикул...";
     String forChangeShouldNameId = "Для этого действия необходимо ввести наименование и артикул продукта, данные которого хотите изменить...";
 
-
-
-    static String name;
-
-
-
+    // переменная для хранения имени пользователя
+    String name;
 
     public static void main(String[] args) {
 
@@ -60,111 +61,153 @@ public class UserUI {
 
     }
 
-
-
+    // метод для знакомства с пользователей
     public void welcome() {
-// вывод в консоль первого сообщения-указания для определения имени пользователя
+
+        // вывод в консоль первого сообщения-указания для определения имени пользователя
         String hello = "Представься...";
         System.out.println(hello);
-// считываение введеных данных и занесение их в переменную name, по которой к пользователю будут обращаться, как по имени
+
+        // считываение введеных данных и занесение их в переменную name, по которой к пользователю будут обращаться, как по имени
         name = scanner.nextLine();
 
+        // вызов метода, отвечающего за вывод меню
         init();
     }
 
+    // метод для вывода стартового меню и обработки команд, относящихся к нему
     public void init() {
-// вывод в консоль второго сообщения-указания для выбора дальнейших действий
-        String whatDo = ", что сделать?";
-        System.out.println(name + whatDo);
 
-// вывод в консоль меню и сообщения-указания "Select:"
-        System.out.println(menuStart + "\nSelect:");
-// переменная для хранения введенного пользователем числа, указывающего на его выбор конкретного пункта меню
-        int answerMenuStart = scanner.nextInt();
-        scanner.nextLine();  // для отчистки буфера от \n
+        // цикл для реализации работы с исключением и возобновления работы программы после возникновения исключения
+        while(true) {
+            // вывод в консоль второго сообщения-указания для выбора дальнейших действий
+            String whatDo = ", что сделать?";
+            System.out.println(name + whatDo);
 
-// передача введенного пользователем числа в Tracker
-        menuChoise(answerMenuStart);
+            // вывод в консоль меню и сообщения-указания "Select:"
+            System.out.println(menuStart + "\nSelect:");
 
+            // проверка на возникновение исключения, если пользователь введет не целое положительное число
+            try {
+                // переменная для хранения введенного пользователем числа, указывающего на его выбор конкретного пункта меню
+                int answerMenuStart = scanner.nextInt(); 
+                scanner.nextLine();  // для отчистки буфера от \n
+                menuChoice(answerMenuStart);  // передача команды пользователя на обработку в метод menuChoice()
+                break;
+            }
+            catch (InputMismatchException e) {  // ловля исключения и вывод в консоль сообщения-указания
+                System.out.println("ВВЕДИ ЧИСЛО");
+                scanner.nextLine();  // для отчистки буфера от \n
+            }
+        }
     }
+    
+    // метод для обработки команды пользователя
+    public void menuChoice(int choice) {
+        
+        // проверка, что введеное пользователем число находится в диапазоне чисел, соответствующих пунктам меню
+        if(choice > 5 || choice < 0) {
 
-    public void menuChoise(int choise) {
-
-        if(choise > 5 || choise < 0) {
+            // сообщение на необходимость введения числа в соответствии с пунктами меню
             System.out.println(mustWrite);
 
+            // возврат к меню
             init();
-
-        } else if (choise == ADD) {
-            System.out.println(youWrite + choise);
+            
+            // введеная команда соответствует пункту меню ADD
+        } else if (choice == ADD) {
+            
+            // вывод сообщения и введенной пользователем команды
+            System.out.println(youWrite + choice);
+            // вывод сообщения о необходимости ввести имя продукта, цену продукта и артикул продукта
             System.out.println(forThisShouldNamePriceId);
+            // сообщение-указание на ввод имени продукта
             System.out.println(name + writeName);
-
-                String nameProduct = scanner.nextLine();
-
-            System.out.println(name + writePrice);
-
-                int priceProduct = scanner.nextInt();
-
-            System.out.println(name + writeId);
-
-                int idProduct = scanner.nextInt();
-
-            tracker.addProduct(new Product(nameProduct, priceProduct, idProduct));
-
-            init();
-
-        } else if (choise == REMOVE) {
-            System.out.println(youWrite + choise);
-            System.out.println(forThisShouldNameId);
-            System.out.println(name + writeName);
+            // переменная для хранения ввденого пользователем имени продукта
             String nameProduct = scanner.nextLine();
+            // сообщение-указание на ввод цены продукта
+            System.out.println(name + writePrice);
+            // переменная для хранения ввденой пользователем цены продукта
+            int priceProduct = scanner.nextInt();
+            // сообщение-указание на ввод артикула продукта
             System.out.println(name + writeId);
+            // переменная для хранения введеного пользователем артикула продукта
             int idProduct = scanner.nextInt();
 
-            tracker.removeProduct(nameProduct ,idProduct);
+            // передача введеных пользователем данных для обработки с введенной командой
+            tracker.addProduct(new Product(nameProduct, priceProduct, idProduct));
 
-
-           // System.out.println("позиция с наименованием \"" + nameProduct + "\" удалена");
-
-
-
+            // возврат к меню
             init();
 
-        } else if (choise == CHANGE) {
-            System.out.println(youWrite + choise);
+            // введеная команда соответствует пункту меню REMOVE
+        } else if (choice == REMOVE) {
+            
+            // вывод сообщения и введенной пользователем команды
+            System.out.println(youWrite + choice);
+            // вывод сообщения о необходимости ввести имя продукта и артикул продукта
+            System.out.println(forThisShouldNameId);
+            // сообщение-указание на ввод имени продукта
+            System.out.println(name + writeName);
+            // переменная для хранения ввденого пользователем имени продукта
+            String nameProduct = scanner.nextLine();
+            // сообщение-указание на ввод артикула продукта
+            System.out.println(name + writeId);
+            // переменная для хранения введеного пользователем артикула продукта
+            int idProduct = scanner.nextInt();
+
+            // передача введеных пользователем данных для обработки с введенной командой
+            tracker.removeProduct(nameProduct ,idProduct);
+
+            // возврат к меню
+            init();
+
+            // введеная команда соответствует пункту меню CHANGE
+        } else if (choice == CHANGE) {
+            
+            System.out.println(youWrite + choice);
             System.out.println(forChangeShouldNameId);
             System.out.println(name + writeName);
             String nameProduct = scanner.nextLine();
             System.out.println(name + writeId);
             int idProduct = scanner.nextInt();
 
+            // передача введеных пользователем данных для обработки с введенной командой
             tracker.changeProduct(nameProduct ,idProduct);
 
+            // возврат к меню
             init();
 
-        } else if (choise == FIND) {
-            System.out.println(youWrite + choise);
+            // введеная команда соответствует пункту меню FIND
+        } else if (choice == FIND) {
+            // вывод сообщения и введенной пользователем команды
+            System.out.println(youWrite + choice);
+            // вывод сообщения о необходимости ввести имя продукта и артикул продукта
             System.out.println(forThisShouldNameId);
+            // сообщение-указание на ввод имени продукта
             System.out.println(name + writeName);
+            // переменная для хранения ввденого пользователем имени продукта
             String nameProduct = scanner.nextLine();
+            // сообщение-указание на ввод артикула продукта
             System.out.println(name + writeId);
+            // переменная для хранения введеного пользователем артикула продукта
             int idProduct = scanner.nextInt();
+            
+            // передача введеных пользователем данных для обработки с введенной командой
             tracker.findProduct(nameProduct, idProduct);
 
+            // возврат к меню
             init();
 
-
-        } else if (choise == SHOWALL) {
+            // введеная команда соответствует пункту меню SHOWALL
+        } else if (choice == SHOWALL) {
             tracker.showAllProduct();
             init();
 
-        } else if (choise == EXIT) {
-            System.out.println(youWrite + choise);
-
+            // введеная команда соответствует пункту меню EXIT
+        } else if (choice == EXIT) {
+            // вывод сообщения и введенной пользователем команды
+            System.out.println(youWrite + choice);
         }
-
     }
-
-
 }
