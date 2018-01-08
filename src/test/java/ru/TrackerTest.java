d0d4a0ce190e5e9a8c24a72df1621d17a7ea4835
cladendas <rvdagi05@yandex.ru>
2018-01-08 19:05:32 +0300

    написал тесты для класса Tracker: тесты на добавление одного и двух продуктов, тесты на удаление одного и двух продуктов

--- src/test/java/ru/TrackerTest.java
+++ src/test/java/ru/TrackerTest.java
@@ -5,16 +5,56 @@ import static org.junit.Assert.assertThat;
 
 public class TrackerTest {
     @Test
-    public void whenUserWantADD() {
+    public void whenADDOneProduct() {
         Tracker tracker = new Tracker();
-        Product prod = new Product("картошка", 1,2);
-        Product pot = new Product("карка", 2,3);
-        tracker.addProduct(prod);
-
+        Product potato = new Product("картошка", 1,2);
+        tracker.addProduct(potato);
+        Product resultProduct = tracker.products[0];
+        assertThat(resultProduct, is(potato));
+    }
+    @Test
+    public void whenADDTwoProduct() {
+        Tracker tracker = new Tracker();
+        Product potato = new Product("картошка", 1,2);
+        tracker.addProduct(potato);
+        Product apple = new Product("яблоко", 25,444);
+        tracker.addProduct(apple);
+        Product resultProduct = tracker.products[1];
+        assertThat(resultProduct, is(apple));
+    }
+    @Test
+    public void whenREMOVEOneProduct() {
+        Tracker tracker = new Tracker();
+        Product potato = new Product("картошка", 1,2);
+        Product prodNULL = null;
+        tracker.addProduct(potato);
+        tracker.removeProduct(potato.getName(),potato.getId());
+        Product resultProduct = tracker.products[0];
+        assertThat(resultProduct, is(prodNULL));
+    }
+    @Test
+    public void whenREMOVETwoProduct() {
+        Tracker tracker = new Tracker();
+        Product potato = new Product("картошка", 1,2);
+        Product apple = new Product("яблоко", 25,444);
+        Product prodNULL = null;
+        tracker.addProduct(potato);
+        tracker.addProduct(apple);
+        tracker.removeProduct(potato.getName(),potato.getId());
+        tracker.removeProduct(apple.getName(),apple.getId());
+        Product resultProductFirst = tracker.products[0];
+        Product resultProductSecond = tracker.products[1];
+        assertThat(resultProductFirst, is(prodNULL));
+        assertThat(resultProductSecond, is(prodNULL));
+    }
+    @Test
+    public void whenFINDOneProduct() {
+        Tracker tracker = new Tracker();
+        Product potato = new Product("картошка", 1,2);
+        tracker.addProduct(potato);
         Product resultProduct = tracker.products[0];
 
-        assertThat(resultProduct, is(prod));
+        assertThat(resultProduct, is(potato));
     }
-
 
 }
