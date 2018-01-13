6d52914d040963fbbbd28204c2688d2154837302
cladendas <rvdagi05@yandex.ru>
2018-01-13 20:04:48 +0300

    в классе Tracker заменил метод changeProduct на changeProductMethod и перенес всю логику этого метода в отдельный класс TrackerChangeProduct

---
+++ src/main/java/ru/TrackerChangeProduct.java
@@ -0,0 +1,85 @@
+package ru;
+
+public class TrackerChangeProduct extends Tracker{
+
+    // переменные для хранения пунктов меню
+    final String menuChangeSectionNAME = ". Наименование\n";
+    final String menuChangeSectionPRICE = ". Цена\n";
+    final String menuChangeSectionID = ". Артикул\n";
+    final String menuChangeSectionCANCEL = ". Отмена\n";
+
+    // переменные для хранения нумерации пунктов меню
+    final int nameChange = 0;
+    final int priceChange = 1;
+    final int idChange = 2;
+    final int cancelChange = 3;
+
+    private int changeProductIndex = -1;
+
+    public void changeProductFindIndex(Product prod) {
+        if (findProduct(prod.getName(), prod.getId()) != -1) {
+
+            changeProductIndex = findProduct(prod.getName(), prod.getId());
+            changeProductMenu();
+        }
+        System.out.println("совпадений не найдено");
+
+        // выводим основное меню
+        new UserUI().init();
+    }
+
+    private void changeProductMenu() {
+
+        // переменная для сбора меню
+        String menuChangeStart = nameChange + menuChangeSectionNAME + priceChange + menuChangeSectionPRICE +
+                idChange + menuChangeSectionID + cancelChange + menuChangeSectionCANCEL;
+
+        // цикл для вывода меню
+        // после выполнения какого-либо из пунктов меню будет выводиться до тех пор,
+        // пока не поступит команда ОТМЕНА
+        System.out.println("Что изменить в найденом продукте...");
+        System.out.println(menuChangeStart);
+
+        int choiceChange = inputInt();
+        changeProductField(choiceChange);
+
+    }
+
+    private void changeProductField(int choiceChange) {
+        if (choiceChange > 3 || choiceChange < 0) {
+            System.out.println("Необходимо указать цифру, соответствующую выбранному полю меню...");
+        } else if (choiceChange == nameChange) {
+
+            System.out.println("веди новое имя...");
+            changeProductName(inputString());
+
+        } else if (choiceChange == priceChange) {
+
+            System.out.println("Введи новую цену...");
+            changeProductPrice(inputInt());
+
+        } else if (choiceChange == idChange) {
+
+            System.out.println("Введи новый артикул...");
+            changeProductId(inputInt());
+
+        } else if (choiceChange == cancelChange) {
+
+        }
+
+    }
+
+    private void changeProductName(String nameNew) {
+        products[changeProductIndex].setName(nameNew);
+    }
+
+    private void changeProductPrice(int priceNew) {
+        products[changeProductIndex].setPrice(priceNew);
+    }
+
+    private void changeProductId(int idNew) {
+        products[changeProductIndex].setPrice(idNew);
+    }
+
+
+}
