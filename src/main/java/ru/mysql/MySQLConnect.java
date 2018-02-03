269dc8d6be716fe004587aaca879370af54273a3
cladendas <rvdagi05@yandex.ru>
2018-02-03 23:55:10 +0300

    попробовал сделать запрос на добавление данных в таблицу

--- src/main/java/ru/mysql/MySQLConnect.java
+++ src/main/java/ru/mysql/MySQLConnect.java
@@ -19,19 +19,22 @@ public class MySQLConnect {
 
             System.out.println("------- Подключение установлено -------\n\n\n");
 
-            // создание объекта для SQl запросов без параметром
+            // создание объекта для SQl запросов без параметров
             Statement statement = null;
             statement = connection.createStatement();
 
-            // выполнение запроса
-            // здесь: вывод всех столбцов таблицы products
-            ResultSet rs = statement.executeQuery("SELECT * FROM products");
+            // выполнение запроса на добавление данных
+            statement.executeUpdate("INSERT INTO products VALUES('виноград','444','66')");
+
+            // запрос на вывод всех столбцов таблицы products
+            ResultSet rs = null;
+            rs = statement.executeQuery("SELECT * FROM products");
 
             // переменная для хранения количества колонок в результирующем запросе
             int columns = rs.getMetaData().getColumnCount();
 
             // цикл для вывода каждой строки таблицы
-            // метод next() используется к следующей строке
+            // метод next() используется для перехода к следующей строке
             while(rs.next()){
                 for (int i = 1; i <= columns; i++){
 
@@ -42,8 +45,7 @@ public class MySQLConnect {
             }
 
         } catch (SQLException ex) {
-            Logger.getLogger(MySQLConnect.class.getName()).log(Level.SEVERE,
-                    null, ex);
+            Logger.getLogger(MySQLConnect.class.getName()).log(Level.SEVERE, null, ex);
         }
          
         if(null != connection) {
